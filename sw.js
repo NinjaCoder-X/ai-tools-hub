6ce8c7192035e945489781f03f46c7de5e52d64d
NinjaCoderX <vvsithum241@gmail.com>
2026-03-20 07:37:10 +0530

    monetag
    added the ads

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10695364
+    "zoneId": 10753068
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
