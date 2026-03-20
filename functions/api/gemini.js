6885723213ebd45f818f7566e5b668aedb7b4b92
NinjaCoderX <vvsithum241@gmail.com>
2026-03-20 06:17:02 +0530

    Update gemini.js

--- functions/api/gemini.js
+++ functions/api/gemini.js
@@ -11,7 +11,7 @@ export async function onRequestPost({ request, env }) {
 
         const body = await request.json();
 
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(body)
